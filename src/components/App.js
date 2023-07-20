9c4576dac3da494abff3f00a1c2c0c901c4776a6
Nitu Dubey <nitutiwari01@gmail.com>
2023-07-19 23:01:32 -0400

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  const fetchData = () => {
+    fetch("https://dummyjson.com/products")
+      .then((response) => {
+        response.json().then((data) => {
+          console.log(data);
+          setData(data);
+        });
+        // console.log(data)
+        //         setData(data);
+      })
+
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
